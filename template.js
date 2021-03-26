df3b86ceb62a225b9f6ac8ac59ef1fa1eaebacc4
Kevin Muessig <Kevin.muessig@sap.com>
2021-03-26 16:21:39 +0100

    Create template.js

---
+++ template.js
@@ -0,0 +1,41 @@
+module.exports = `
+# Hi there 👋 👨🏻‍💻
+
+My name is Kevin and I'm currently working as a Developer Advocate in the [SAP Developer Advocacy](https://developers.sap.com/) team at SAP.
+I love connecting with interesting and cool people all around the world 🌎 to talk about Tech, Culture and whatever connects us!
+
+Not only am I crazy passionate about coding but I also do a lot of different types of sports. I like being outdoors, being with people and I go crazy when it comes to music!
+
+## Connect with me here
+
+- [SAP Community](https://people.sap.com/kevin.muessig) 
+- [Twitter](https://twitter.com/kevinmuessig)
+- [LinkedIn](https://www.linkedin.com/in/kevinmuessig/)
+
+## Who doesn't like content?
+
+<table><tr><td valign="top" width="50%">
+ 
+## On my blog
+{{#items}}- [{{{title}}}]({{{id}}}) 
+{{/items}} 
+- More on [My SAP Community Blog](https://people.sap.com/thomas.jung#content:blogposts)
+</td>
+  
+<td valign="top" width="50%">
+  
+## Videos and Live Streams
+{{#itemsNew}}- [{{{title}}}]({{{link}}}) ({{date}})
+{{/itemsNew}}
+- More on [SAP Developers YouTube Channel](https://www.youtube.com/channel/UCNfmelKDrvRmjYwSi9yvrMg)
+</td></tr></table>
+
+
+![README builder](https://github.com/jung-thomas/jung-thomas/workflows/README%20builder/badge.svg)
+
+<a href="https://github.com/anuraghazra/github-readme-stats">
+  <img align="center" src="https://github-readme-stats.vercel.app/api?username=KevinMuessig&count_private=true&show_icons=true&theme=bear" />
+</a>
+<a href="https://github.com/anuraghazra/github-readme-stats">
+  <img align="center" src="https://github-readme-stats.vercel.app/api/top-langs/?username=KevinMuessig&show_icons=true&theme=bear" />
+</a>
