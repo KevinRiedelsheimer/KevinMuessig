d033b37756f89623d7ec26b19489e3b4bedb506e
Kevin Muessig <Kevin.muessig@sap.com>
2021-03-26 16:38:34 +0100

    Update template.js

--- template.js
+++ template.js
@@ -1,10 +1,17 @@
 module.exports = `
+
 # Hi there 👋 👨🏻‍💻
 
 My name is Kevin and I'm currently working as a Developer Advocate in the [SAP Developer Advocacy](https://developers.sap.com/) team at SAP.
-I love connecting with interesting and cool people all around the world 🌎 to talk about Tech, Culture and whatever connects us!
+I love connecting with interesting and cool people all around the world 🌎 to talk about Tech, Culture and whatever is connecting us!
 
-Not only am I crazy passionate about coding but I also do a lot of different types of sports. I like being outdoors, being with people and I go crazy when it comes to music!
+Not only am I passionate about coding but also do a lot of different types of sports. Being outdoors, socializing and making music makes me smile and happy!
+
+My Hobbies are:
+- Rock Climbing 🧗🏻
+- Boxing 🥊
+- Golf 🏌🏻
+- Making Music 🎧
 
 ## Connect with me here
 
